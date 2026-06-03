2056170f2d6557c5e9a48acc508f64fbedb8b8f3
GabrielF <gferrazsiqueira05@gmail.com>
2026-06-02 22:48:25 -0300

    result2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -7,6 +7,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     result = split_integer(value, number_of_parts)
     assert sum(result) == value
 
+
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     value = 6
     number_of_parts = 2
